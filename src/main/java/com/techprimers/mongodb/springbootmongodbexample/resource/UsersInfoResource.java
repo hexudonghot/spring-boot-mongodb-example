d6ac2071588092e575fb565ce9c5591402b251ce
何旭東 <>
2019-05-09 15:49:40 +0800

    mongodb 提交

--- src/main/java/com/techprimers/mongodb/springbootmongodbexample/resource/UsersInfoResource.java
+++ src/main/java/com/techprimers/mongodb/springbootmongodbexample/resource/UsersInfoResource.java
@@ -1,6 +1,6 @@
 package com.techprimers.mongodb.springbootmongodbexample.resource;
 
-import com.techprimers.mongodb.springbootmongodbexample.document.Qqinfo;
+import com.mongodb.BasicDBObject;
 import com.techprimers.mongodb.springbootmongodbexample.document.Viewtemplate;
 import com.techprimers.mongodb.springbootmongodbexample.repository.UserInfoRepository;
 import org.slf4j.Logger;
@@ -14,6 +14,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author yzl
+ */
 @RestController
 @RequestMapping("/rest/usersinfo")
 public class UsersInfoResource {
@@ -28,31 +34,14 @@ public class UsersInfoResource {
         this.userInfoRepository = userRepository;
     }
 
-    @GetMapping("/add")
-    public void add()
-    {
-
-    }
-
-    @GetMapping("/del")
-    public Qqinfo del()
-    {
-
-//        Update update = new Update();
-//        update.pull("template",new BasicDBObject("id",templateId));
-//        Query query = Query.query(Criteria.where("id").is(viewTemplateId));
-//         db.updateFirst(query,update, ViewTemplate.class);
-        return null;
-    }
-
 
     @GetMapping("/update")
     public void update()
     {
 
         Update update =new Update();
-        update.set("template.$.view", "fuck");
-        update.set("template.$.image", "fuck you");
+        update.set("template.$.view", "fuck11");
+        update.set("template.$.image", "fuck you2222");
         Query query = Query.query(new Criteria().andOperator(Criteria.where("_id").is("12"),Criteria.where("template").elemMatch(Criteria.where("_id").is("33"))));
         template.updateFirst(query, update, Viewtemplate.class);
     }
@@ -60,27 +49,40 @@ public class UsersInfoResource {
     @GetMapping("/find")
     public Viewtemplate find()
     {
-//        long t1 = System.currentTimeMillis();
-//        Query query=new Query();
-//        Criteria first2 =Criteria.where("$userId").is(11);
-//        Criteria first1 = Criteria.where("_id").is(11).elemMatch(first2);
-//        query.addCriteria(first1);
-//        Userinfo info = template.findOne(query,Userinfo.class);
-//        long t2 = System.currentTimeMillis();
-//        System.out.println(t2-t1);
-//        Query query=new Query();
-//        Criteria first1 = Criteria.where("_id").is(11);
-//        query.addCriteria(first1);
-//        Qqinfo info = template.findOne(query, Qqinfo.class);
-
-
         Query query = Query.query(new Criteria().andOperator(Criteria.where("_id").is("12"),Criteria.where("template").elemMatch(Criteria.where("_id").is("33"))));
-
-
-
-        //Query query2=new Query(Criteria.where("_id").is(11).and("data.userId").is(0));
         Viewtemplate info= template.findOne(query,Viewtemplate.class);
         return info;
+    }
+
+
+    @GetMapping("/add")
+    public void add()
+    {
+        /**
+         * db.getCollection('user_info').update(
+         * {"_id": ObjectId("5cd0f42a39e94fbcf6940da3")},
+         * {$push: { data: { userId: 2,cont:"xxxx",sec:10,userId:98988}}})
+         */
+
+        Map mp = new HashMap();
+        mp.put("_id","444");
+        mp.put("view","5555");
+        mp.put("image","66666");
+        Update update = new Update();
+        update.push("template",new BasicDBObject(mp));
+        Query query = Query.query(Criteria.where("_id").is("33"));
+        template.updateFirst(query,update,Viewtemplate.class);
+    }
+
+
+
+    @GetMapping("/del")
+    public void del()
+    {
+        Update update = new Update();
+        update.pull("template",new BasicDBObject("_id","333"));
+        Query query = Query.query(Criteria.where("_id").is("33"));
+        template.updateFirst(query,update,Viewtemplate.class);
     }
 
 }
