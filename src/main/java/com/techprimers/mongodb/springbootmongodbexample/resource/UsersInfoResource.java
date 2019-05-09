948c8aaa4ed924d0bfeb5880e9e0e2bcd910e449
何旭東 <>
2019-05-09 16:33:37 +0800

    mongodb 提交

--- src/main/java/com/techprimers/mongodb/springbootmongodbexample/resource/UsersInfoResource.java
+++ src/main/java/com/techprimers/mongodb/springbootmongodbexample/resource/UsersInfoResource.java
@@ -11,6 +11,7 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,65 +24,53 @@ import java.util.Map;
 @RestController
 @RequestMapping("/rest/usersinfo")
 public class UsersInfoResource {
-
     private static final Logger LOGGER = LoggerFactory.getLogger(MongoTemplate.class);
-
     private UserInfoRepository userInfoRepository;
     @Autowired
     private MongoTemplate template;
-
     public UsersInfoResource(UserInfoRepository userRepository) {
         this.userInfoRepository = userRepository;
     }
-
-
     @GetMapping("/update")
-    public void update()
+    public void update(String fk, String img, String id1,String id2)
     {
-
         Update update =new Update();
-        update.set("template.$.view", "fuck11");
-        update.set("template.$.image", "fuck you2222");
-        Query query = Query.query(new Criteria().andOperator(Criteria.where("_id").is("12"),Criteria.where("template").elemMatch(Criteria.where("_id").is("33"))));
+        update.set("template.$.view", fk);
+        update.set("template.$.image", img);
+        Query query = Query.query(new Criteria().andOperator(Criteria.where("_id").is(id1),Criteria.where("template").elemMatch(Criteria.where("_id").is(id2))));
         template.updateFirst(query, update, Viewtemplate.class);
     }
 
     @GetMapping("/find")
-    public Viewtemplate find()
+    public Viewtemplate find(String id)
     {
-        Query query = Query.query(new Criteria().andOperator(Criteria.where("_id").is("12"),Criteria.where("template").elemMatch(Criteria.where("_id").is("33"))));
+        Query query = Query.query(new Criteria().andOperator(Criteria.where("_id").is(id)));
         Viewtemplate info= template.findOne(query,Viewtemplate.class);
         return info;
     }
 
 
     @GetMapping("/add")
-    public void add()
+    public void add(String id1,String id2,String view,String image)
     {
-        /**
-         * db.getCollection('user_info').update(
-         * {"_id": ObjectId("5cd0f42a39e94fbcf6940da3")},
-         * {$push: { data: { userId: 2,cont:"xxxx",sec:10,userId:98988}}})
-         */
-
         Map mp = new HashMap();
-        mp.put("_id","444");
-        mp.put("view","5555");
-        mp.put("image","66666");
+        mp.put("_id",id2);
+        mp.put("view",view);
+        mp.put("image",image);
         Update update = new Update();
         update.push("template",new BasicDBObject(mp));
-        Query query = Query.query(Criteria.where("_id").is("33"));
+        Query query = Query.query(Criteria.where("_id").is(id1));
         template.updateFirst(query,update,Viewtemplate.class);
     }
 
 
 
     @GetMapping("/del")
-    public void del()
+    public void del(String id1,String id2)
     {
         Update update = new Update();
-        update.pull("template",new BasicDBObject("_id","333"));
-        Query query = Query.query(Criteria.where("_id").is("33"));
+        update.pull("template",new BasicDBObject("_id",id2));
+        Query query = Query.query(Criteria.where("_id").is(id1));
         template.updateFirst(query,update,Viewtemplate.class);
     }
 
